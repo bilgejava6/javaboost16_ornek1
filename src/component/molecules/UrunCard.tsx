45c528b3d99eb3e1f62297839ee1b638dd1f48d4
bilgejava6 <bilgejava6@gmail.com>
2025-01-30 11:01:17 +0300

    ürün listesi eklendi ve propslar aktarıldı.

--- src/component/molecules/UrunCard.tsx
+++ src/component/molecules/UrunCard.tsx
@@ -1,20 +1,29 @@
 import React from 'react'
-
-function UrunCard() {
+/**
+ * Soru: Java da bir kod öbeğini method haline getirdiniz, bu methodu çağırarak başka bir yerde 
+ * defalarca kullanabilirsiniz. Peki her kullanıldığı yerde farklı tepkiler vermesini istiyorsanız ne yaparsınız?
+ * 
+ * DİKKAT!!!!
+ * JS de değişkenlerin türünü tanımlamak zorunda değilsiniz. ancak
+ * TS de değişkenlerin türünü tanımlamak zorundasınız.
+ * any -> her türü ifade eder. Java da ki Object türüne karşılık gelir.
+ */
+function UrunCard(props: any) {
+  
   return (
     <div className="col-4 mb-2">
         <div className="card shadow">
-            <img height={350} src="https://productimages.hepsiburada.net/s/349/424-600/110000357018013.jpg/format:webp" className="card-img-top p-4" />
+            <img height={350} src={props.resim} className="card-img-top p-4" />
             <div className="card-body">
-                <h5 className="card-title">Msı Mag B760 Tomahawk</h5>
+                <h5 className="card-title">{props.ad}</h5>
                 <p className="card-text">
-                    Yeni nesil teknolojiyi evinize getiren MSI MAG B760 Tomahawk WiFi anakart ile tanışın. Bu anakart, performans ve stilin mükemmel bir birleşimini sunarak, bilgisayar deneyiminizi bir üst seviyeye taşıyor
+                    {props.aciklama}
                 </p>
             </div>
             <ul className="list-group list-group-flush">
-                <li className="list-group-item text-danger">7.847,00 TL</li>
-                <li className="list-group-item">30 günün en düşük fiyatı</li>
-                <li className="list-group-item text-bg-warning"><small>Tahmini 1 Şubat Cumartesi kapında</small></li>
+                <li className="list-group-item text-danger">{props.urunFiyati}</li>
+                <li className="list-group-item">{props.promosyon}</li>
+                <li className="list-group-item text-bg-warning"><small>{props.teslimat}</small></li>
             </ul>
             <div className="card-body text-end">
                 <input type='button' className="btn btn-warning" value={'Sepete Ekle'} />                           
